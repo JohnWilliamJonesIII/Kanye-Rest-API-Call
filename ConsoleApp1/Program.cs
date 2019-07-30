044463c6b214cce77e8d497d38501bdbe41355f0
John <johnwilliamjonesiii@gmail.com>
2019-07-30 14:41:26 -0500

    Finished Kanye West API call

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Mark8InClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = " https://api.kanye.rest";
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+
+            int quoteStartIndex = response.IndexOf("value") + 8;
+            int quoteLength = response.Length - quoteStartIndex - 1;
+            string kanyeQuote = response.Substring(quoteStartIndex, quoteLength);
+            Console.WriteLine("From the mind of Kanye West" + kanyeQuote + '.');
+        }
+    }
+}
